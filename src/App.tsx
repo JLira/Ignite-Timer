6a913a3e39d3dc5c1933fa1dec3c75ff619ebdd9
jlira <jobson.lira@gmail.com>
2023-10-12 01:38:04 -0300

    Contexto entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaulTheme } from './styles/themes/default'
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CycleContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaulTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,7 +1,7 @@
+import { differenceInSeconds } from 'date-fns'
 import { useContext, useEffect } from 'react'
 import { CountdownContainer, Separator } from './styles'
-import { differenceInSeconds } from 'date-fns'
-import { CyclesContext } from '../..'
+import { CyclesContext } from '../../../../contexts/CycleContext'
 
 export function Countdown() {
   const {
@@ -11,6 +11,7 @@ export function Countdown() {
     amountSecondsPassed,
     setSecondsPassed,
   } = useContext(CyclesContext)
+
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
   useEffect(() => {
